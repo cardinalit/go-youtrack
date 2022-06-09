bc3c85bfd7ec153f5756a95281ac892c20e83874
CardinalIT <mk@imagespark.ru>
2022-06-09 14:30:49 +0300

    [+] UPD: added ListIssues

--- issues.go
+++ issues.go
@@ -1,99 +1,115 @@
 package youtrack
 
 import (
-	"context"
-	"encoding/base64"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/url"
+   "context"
+   "encoding/base64"
+   "fmt"
+   "io"
+   "io/ioutil"
+   "log"
+   "net/url"
 )
 
 type ProjectID struct {
-	ID string `json:"id"`
+   ID string `json:"id"`
 }
 
 type Issue struct {
-	Summary     string    `json:"summary"`
-	Description string    `json:"description"`
-	Project     ProjectID `json:"project"`
+   Summary     string    `json:"summary"`
+   Description string    `json:"description"`
+   Project     ProjectID `json:"project"`
 }
 
 type IDResult struct {
-	ID string `json:"id"`
+   ID string `json:"id"`
 }
 
 type IssueResult struct {
-	IDResult
-	NumberInProject int `json:"numberInProject"`
+   IDResult
+   NumberInProject int `json:"numberInProject"`
+}
+
+// ListIssues returns a list of Projects the user has access to.
+// For more information, see
+// https://www.jetbrains.com/help/youtrack/incloud/2019.3/resource-api-admin-projects.html
+func (api *Api) ListIssues(ctx context.Context) ([]Issue, error) {
+   var Issues []Issue
+   u := &url.URL{
+      Path:     "issues",
+      RawQuery: "fields=id,project,description,summary",
+   }
+   err := api.Get(ctx, u, &Issues)
+   if err != nil {
+      return nil, err
+   }
+   return Issues, nil
 }
 
 // IssueURL returns a user facing (rather than REST API) URL to the issue.
 // Note that because this issue uses the short project name in the URL, the link
 // could be broken if the project short name changes.
 func IssueURL(baseURL *url.URL, shortProjectName string, issueNumberInProject int) *url.URL {
-	path := fmt.Sprintf("../issue/%s-%d", shortProjectName, issueNumberInProject)
-	return baseURL.ResolveReference(&url.URL{Path: path})
+   path := fmt.Sprintf("../issue/%s-%d", shortProjectName, issueNumberInProject)
+   return baseURL.ResolveReference(&url.URL{Path: path})
 }
 
 func (api *Api) IssueURL(shortProjectName string, issueNumberInProject int) *url.URL {
-	return IssueURL(api.BaseURL, shortProjectName, issueNumberInProject)
+   return IssueURL(api.BaseURL, shortProjectName, issueNumberInProject)
 }
 
 // CreateIssue returns the issue ID on success.
 func (api *Api) CreateIssue(ctx context.Context, project, summary, description string) (*IssueResult, error) {
-	issue := &Issue{
-		Summary:     summary,
-		Description: description,
-		Project: ProjectID{
-			ID: project,
-		},
-	}
+   issue := &Issue{
+      Summary:     summary,
+      Description: description,
+      Project: ProjectID{
+         ID: project,
+      },
+   }
 
-	result := new(IssueResult)
-	u := &url.URL{
-		Path:     "issues",
-		RawQuery: "fields=id,numberInProject",
-	}
+   result := new(IssueResult)
+   u := &url.URL{
+      Path:     "issues",
+      RawQuery: "fields=id,numberInProject",
+   }
 
-	err := api.Post(ctx, u, issue, result)
-	if err != nil {
-		return nil, err
-	}
+   err := api.Post(ctx, u, issue, result)
+   if err != nil {
+      return nil, err
+   }
 
-	return result, nil
+   return result, nil
 }
 
 type IssueAttachment struct {
-	Name          string `json:"name"`
-	Base64Content string `json:"base64Content"`
+   Name          string `json:"name"`
+   Base64Content string `json:"base64Content"`
 }
 
 // CreateIssueAttachment attached a file to the given issue ID. On success, the attachment ID is returned.
 func (api *Api) CreateIssueAttachment(ctx context.Context, issueID string, attachment io.Reader, name, mediaType string) (string, error) {
 
-	data, err := ioutil.ReadAll(attachment)
-	if err != nil {
-		log.Print("Failed to read attachment", err)
-		return "", err
-	}
+   data, err := ioutil.ReadAll(attachment)
+   if err != nil {
+      log.Print("Failed to read attachment", err)
+      return "", err
+   }
 
-	issueAttachment := &IssueAttachment{
-		Name:          name,
-		Base64Content: "data:" + mediaType + ";base64," + base64.StdEncoding.EncodeToString(data),
-	}
+   issueAttachment := &IssueAttachment{
+      Name:          name,
+      Base64Content: "data:" + mediaType + ";base64," + base64.StdEncoding.EncodeToString(data),
+   }
 
-	u := &url.URL{
-		Path: fmt.Sprintf("issues/%s/attachments", issueID),
-	}
-	result := &IDResult{}
+   u := &url.URL{
+      Path: fmt.Sprintf("issues/%s/attachments", issueID),
+   }
+   result := &IDResult{}
 
-	err = api.Post(ctx, u, issueAttachment, result)
-	if err != nil {
-		log.Print("Failed to post attachment.", err)
-		return "", err
-	}
+   err = api.Post(ctx, u, issueAttachment, result)
+   if err != nil {
+      log.Print("Failed to post attachment.", err)
+      return "", err
+   }
 
-	return result.ID, nil
+   return result.ID, nil
 }
